34943e63abbe4c46d31be88990c0904c2d0c61a5
DonRyuDragoni <dragoni.ryu@gmail.com>
2016-10-03 17:03:15 -0300

    Build the image using multiple goroutines

--- image/mandelbrot.go
+++ image/mandelbrot.go
@@ -1,5 +1,9 @@
 package image
 
+import (
+	"sync"
+)
+
 type MandelbrotSetImage struct {
 	Img *Image
 }
@@ -49,21 +53,71 @@ func DefaultColorFn(x, y, itersDone, maxItersPerPixel int) (colors [4]uint8) {
 	return
 }
 
-func NewMandelbrot(size_x, size_y, maxItersPerPixel int, colorFn func(int, int,
-	int, int) [4]uint8) (m *MandelbrotSetImage) {
+/*
+Split the interval [0, `endPoint`] into `length` equaly-spaced points.
+*/
+func splitCols(endPoint, length int) []int {
+	grouplist := make([]int, length)
+
+	for start, i := 0, 0; i < length; i++ {
+		grouplist[i] = start
+		start += endPoint / length
+	}
+
+	return grouplist
+}
+
+/*
+Basically the same as splitCols, but returns a slice of `[start, end]`
+representing the initial and the final points present in each ith interval.
+*/
+func pointList(endPoint, length int) [][2]int {
+	points := make([][2]int, length)
+	s := append(splitCols(endPoint, length), endPoint)
+
+	for i := 0; i < len(s)-1; i++ {
+		if i == 0 {
+			points[i][0] = s[i]
+		} else {
+			points[i][0] = s[i] + 1
+		}
+
+		points[i][1] = s[i+1]
+	}
+
+	return points
+}
+
+type ColorFn func(int, int, int, int) [4]uint8
+
+func NewMandelbrot(size_x, size_y, maxItersPerPixel int,
+	colorer ColorFn, numThreads int) (m *MandelbrotSetImage) {
 	m = new(MandelbrotSetImage)
 	m.Img = NewImage(size_x, size_y)
 
-	for x := 0; x <= m.Img.Width; x++ {
-		for y := 0; y <= m.Img.Height; y++ {
-			n := pointTendsToinfinity(x, y, m.Img.Width, m.Img.Height,
-				maxItersPerPixel)
+	threadPoints := pointList(m.Img.Width, numThreads)
 
-			color := colorFn(x, y, n, maxItersPerPixel)
+	var wg sync.WaitGroup
+	wg.Add(numThreads)
 
-			m.Img.SetPixel(x, y, color[0], color[1], color[2], color[3])
-		}
+	for _, p := range threadPoints {
+		go func(startx, endx int) {
+			for x := startx; x <= endx; x++ {
+				for y := 0; y <= m.Img.Height; y++ {
+					n := pointTendsToinfinity(x, y, m.Img.Width, m.Img.Height,
+						maxItersPerPixel)
+
+					color := colorer(x, y, n, maxItersPerPixel)
+
+					m.Img.SetPixel(x, y, color[0], color[1], color[2], color[3])
+				}
+			}
+
+			wg.Done()
+		}(p[0], p[1])
 	}
+
+	wg.Wait()
 
 	return
 }
--- main.go
+++ main.go
@@ -8,14 +8,15 @@ import (
 
 var (
 	imgfile          = "img.png"
-	size_y           = 2000
+	size_y           = 3000
 	size_x           = size_y * 16 / 9
 	maxItersPerPixel = 500
+	threads          = 4
 )
 
 func main() {
 	img := image.NewMandelbrot(size_x, size_y, maxItersPerPixel,
-		image.DefaultColorFn)
+		image.DefaultColorFn, threads)
 
 	err := img.Img.Encode(imgfile, true)
 	if err != nil {
